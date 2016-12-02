d44010f1982e733156ccd369338379e8fa30d89e
Erik Dzul <erikdzul@hotmail.com>
2016-12-02 11:04:50 -0600

    Added AzureClient

---
+++ XamarinDiplomado/XamarinDiplomado/Models/AzureClient.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinDiplomado.Models
+{
+    public class AzureClient
+    {
+        private IMobileServiceClient _azureClient;
+        private IMobileServiceTable<Language> _table;
+
+        public AzureClient()
+        {
+            _azureClient = new MobileServiceClient("http://midemo.azurewebsites.net");
+            _table = _azureClient.GetTable<Language>();
+        }
+
+        public Task<IEnumerable<Language>> GetLanguages()
+        {
+            return _table.ToEnumerableAsync();
+        }
+    }
+}
---
+++ XamarinDiplomado/XamarinDiplomado/Models/Language.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinDiplomado.Models
+{
+    [DataTable("Languages")]
+    public class Language
+    {
+        [JsonProperty]
+        public string Id { get; set; }
+
+        [JsonProperty]
+        public string Name { get; set; }
+        
+        [JsonProperty]
+        public string Imperative { get; set; }
+
+        [JsonProperty]
+        public string ObjectOriented { get; set; }
+
+        [JsonProperty]
+        public string Functional { get; set; }
+        
+        [JsonProperty]
+        public string Procedural { get; set; }
+        [JsonProperty]
+        public string Generic { get; set; }
+        [JsonProperty]
+        public string Reflective { get; set; }
+        [JsonProperty]
+        public string EventDriven { get; set; }
+    }
+}
